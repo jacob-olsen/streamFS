f2025ae5b8258fb7959ef8d6a9691b02a71bed10
jacob <j.Meinertz@outlook.com>
2026-02-14 19:01:09 +0100

    addet vitureal files

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -14,7 +16,6 @@ import (
 
 func main() {
 	mountPoint := flag.String("mnt", "./mnt", "Directory to mount the filesystem")
-	dataDir := flag.String("data", "./real_data", "Directory to stream data from")
 	debug := flag.Bool("debug", false, "Enable FUSE debug logging")
 	flag.Parse()
 
@@ -22,10 +23,7 @@ func main() {
 		log.Fatalf("Could not create mount directory: %v", err)
 	}
 
-	root, err := fs.NewLoopbackRoot(*dataDir)
-	if err != nil {
-		log.Fatalf("Failed to init loopback root: %v", err)
-	}
+	root := &StreamRoot{}
 
 	sec := time.Second
 	opts := &fs.Options{
@@ -44,9 +42,8 @@ func main() {
 		log.Fatalf("Mount failed: %v", err)
 	}
 
-	log.Printf("🚀 StreamFS is live!")
-	log.Printf("   ├── Mount: %s", *mountPoint)
-	log.Printf("   └── Source: %s", *dataDir)
+	log.Printf("StreamFS is live!")
+	log.Printf("Mount: %s", *mountPoint)
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
@@ -58,4 +55,48 @@ func main() {
 	}()
 
 	server.Wait()
+}
+
+type StreamRoot struct {
+	fs.Inode
+}
+
+func (f *StreamRoot) Readdir(ctx context.Context) (fs.DirStream, syscall.Errno) {
+	var entries []fuse.DirEntry
+
+	entries = append(entries, fuse.DirEntry{
+		Name: "remote_game_file.dat",
+		Mode: fuse.S_IFREG,
+		Ino:  10,
+	})
+
+	entries = append(entries, fuse.DirEntry{
+		Name: "local_cache_log.txt",
+		Mode: fuse.S_IFREG,
+		Ino:  11,
+	})
+
+	return fs.NewListDirStream(entries), 0
+}
+
+func (r *StreamRoot) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
+
+	if name == "remote_game_file.dat" || name == "local_cache_log.txt" {
+
+		fileLogic := &fs.MemRegularFile{
+			Data: []byte("Data for " + name),
+			Attr: fuse.Attr{Mode: 0644},
+		}
+		fmt.Println(r.StableAttr().Ino)
+
+		stable := fs.StableAttr{Mode: fuse.S_IFREG}
+		child := r.NewPersistentInode(ctx, fileLogic, stable)
+
+		out.Attr.Mode = fuse.S_IFREG | 0644
+		out.Attr.Size = uint64(len("Data for " + name))
+
+		return child, 0
+	}
+
+	return nil, syscall.ENOENT
 }
